e7abdabd9adda4029bef89ed2d9f4e26045e717f
BrookeKelly-coder <86648602+BrookeKelly-coder@users.noreply.github.com>
2021-07-08 16:28:33 +1000

    Python session 1

---
+++ variables_playground.py
@@ -0,0 +1,44 @@
+day = "Saturday"
+print(type(day))
+
+message = f"Today is {day}!"
+print(message)
+
+month = "July"
+print(type(month))
+message2 = f"Today is {day} in {month}"
+print(message2)
+
+name = "Carlie"
+message3 = f"Hello {name}"
+print (message3)
+
+#Integers
+#Run distance in m
+run1_dist = 1400
+run2_dist = 1800
+
+#Addition
+total_distance = run1_dist + run2_dist
+
+#Floats
+#Run distance in km
+run3_dist = 1.7
+run4_dist = 1.35
+
+#Addition
+total_distance = run3_dist + run4_dist
+
+#Division and Multiplication
+print(run1_dist / 1000)
+print(run4_dist * 1000)
+
+goal_distance = 20
+current_distance = 8
+
+remaining_distance = goal_distance - current_distance   
+print(remaining_distance)
+
+run5_dist = "5000"
+# print(run5_dist + 3)
+print(int(run5_dist) + 3)
